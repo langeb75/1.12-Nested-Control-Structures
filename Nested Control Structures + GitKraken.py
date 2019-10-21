fae9a6583fab656aea12d7decfab342a3098cd51
Brad Lange <bradlange75@gmail.com>
2019-10-21 14:11:10 -0400

    Add Double For Loop

--- Nested Control Structures + GitKraken.py
+++ Nested Control Structures + GitKraken.py
@@ -3,3 +3,10 @@ Programer: Mr. Lange
 Date: 10.17.19
 Program: Double For Loop
 '''
+
+# This program will have both an inner and outer for loop
+
+for i in range(3):
+    print("Outer For Loop: " + str(i))
+    for l in range(2):
+        print("     Inner For Loop: " + str(l))
