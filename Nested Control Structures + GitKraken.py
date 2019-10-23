bc3c48277160147d053054ae465af8153354eb8d
Brad Lange <bradlange75@gmail.com>
2019-10-23 14:09:24 -0400

    Initial Commit - For Loop + While Loop

--- Nested Control Structures + GitKraken.py
+++ Nested Control Structures + GitKraken.py
@@ -10,3 +10,13 @@ for i in range(3):
     print("Outer For Loop: " + str(i))
     for l in range(2):
         print("     Inner For Loop: " + str(l))
+
+
+'''
+Programmer: Mr. Lange
+Date: 10.23.19
+Program: For Loop + While Loop
+
+This program will incorporate a For Loop embedding a While Loop in it
+'''
+
